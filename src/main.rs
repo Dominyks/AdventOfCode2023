f8bdc5de216179f8101e6d765e67f8c793818c28
d.jonusas <d.jonusas@cid.com>
2023-12-04 10:26:41 +0200

    2023 day 3

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 use file_parser;
 use day_1;
 use day_2;
+use day_3;
 
 fn main() {
-    // let input = file_parser::parse_file(".//input_data//day2//input_sample.txt");
-    let input = file_parser::parse_file(".//input_data//day2//input.txt");
+    // let input = file_parser::parse_file(".//input_data//day3//input_sample.txt");
+    let input = file_parser::parse_file(".//input_data//day3//input.txt");
     let mut lines = input.lines();
 
     // let day_2_ans = day_1::part_1(&mut lines.clone());
     // let day_1_ans = day_1::part_2(&mut lines.clone());
 
-    let day_1_ans = day_2::part_1(&mut lines.clone());
-    let day_2_ans = day_2::part_2(&mut lines.clone());
+    let day_1_ans = day_3::part_1(&mut lines.clone());
+    let day_2_ans = day_3::part_2(&mut lines.clone());
 
     println!("{:?}, {:?}", day_1_ans, day_2_ans);
 }
